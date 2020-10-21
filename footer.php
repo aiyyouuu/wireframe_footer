9f3f91aab68ecba7f0b97b61fdeefa2556e00098
aiyyouuu <dyahayusuciilhami56@yahoo.com>
2020-10-21 21:02:53 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Footer</title>
+
+  <!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<!-- Google Fonts -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+<!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="animate.css">
+
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color darken-2 pt-4">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-sm-10 mx-auto" align="center">
+
+        <!-- Content -->
+        <h5 class="animated bounceInUp font-weight-bold text-uppercase mt-3 mb-4 " style="font-size: 30px">Sponsorship dan Media Partner</h5>
+      
+        <ul class="list-unstyled list-inline text-center">
+        <li class="list-inline-item">
+          <p><input type="image" src="image/sponsor.png" width="80px" height="65px"></p>
+        </li>
+        <li class="list-inline-item">
+          <p><input type="image" src="image/sponsor.png" width="80px" height="65px"></p>
+        </li>
+        <li class="list-inline-item">
+          <p><input type="image" src="image/sponsor.png" width="80px" height="65px"></p>
+        </li>
+        <li class="list-inline-item">
+          <p><input type="image" src="image/sponsor.png" width="80px" height="65px"></p>
+        </li>
+        <li class="list-inline-item">
+          <p><input type="image" src="image/sponsor.png" width="80px" height="65px"></p>
+        </li>
+        <li class="list-inline-item">
+          <p><input type="image" src="image/sponsor.png" width="80px" height="65px"></p>
+        </li>
+      </ul><!-- 
+
+        <p>Contact Person Sponsorship: ....</p> -->
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <hr>
+
+  <!-- Call to action -->
+  <ul class="list-unstyled list-inline text-center py-2">
+   <!--  <li class="list-inline-item">
+      <h5 class="mb-1">Register for free</h5>
+    </li> -->
+    <li class="list-inline-item" >
+      <a href="#!" class="btn btn-dark btn-rounded" style="font-size: 18px" >Verde</a>
+
+    </li>
+  </ul>
+  <!-- Call to action -->
+
+  <hr>
+
+  <!-- Social buttons -->
+  <ul class="list-unstyled list-inline text-center">
+    <p><input type="image" src="image/logo.png" width="270px" height="170px"></p>
+  </ul>
+  <!-- Social buttons -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© VIP UPNYK 2020
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
